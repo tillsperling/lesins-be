1a29e80577930e7c86ab4e10b668b317972b08de
tillsperling <hi@till-sperling.de>
2025-11-04 23:29:38 +0100

    Add validation with `class-validator` and `class-transformer`, and create DTOs for Ingredients, Instructions, Subrecipes, and Recipes modules

--- src/modules/recipes/dto/create-recipe-dto.ts
+++ src/modules/recipes/dto/create-recipe-dto.ts
@@ -1,5 +1,9 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
+import { CreateIngredientDto } from '@/modules/ingredients/dto/create-ingredient-dto';
+import { CreateInstructionDto } from '@/modules/instructions/dto/create-instruction-dto';
+import { CreateSubRecipeDto } from '@/modules/subrecipes/dto/create-subrecipe-dto';
+
 export class CreateRecipeDto {
     @IsString()
     @IsNotEmpty()
