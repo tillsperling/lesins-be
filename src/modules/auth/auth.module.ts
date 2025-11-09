a1eca14856440dd6531b56c7bf2ad920a94ebbed
tillsperling <hi@till-sperling.de>
2025-11-09 12:48:57 +0100

    Add AuthModule with `JwtStrategy` and extend RecipesModule with additional entities

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+
+import { JwtStrategy } from '@/modules/auth/jwt.strategy';
+
+@Module({
+    imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+    providers: [JwtStrategy],
+    exports: [PassportModule],
+})
+export class AuthModule {}
--- src/modules/recipes/recipes.module.ts
+++ src/modules/recipes/recipes.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Ingredient } from '@/modules/recipes/entities/ingredient.entity';
+import { Instruction } from '@/modules/recipes/entities/instruction.entity';
 import { Recipe } from '@/modules/recipes/entities/recipe.entity';
+import { SubRecipe } from '@/modules/recipes/entities/subrecipe.entity';
 import { RecipesController } from '@/modules/recipes/recipes.controller';
 import { RecipesService } from '@/modules/recipes/recipes.service';
 import { RecipeRepository } from '@/modules/recipes/repositories/recipes.repository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Recipe])],
+    imports: [TypeOrmModule.forFeature([Recipe, SubRecipe, Ingredient, Instruction])],
     controllers: [RecipesController],
     providers: [RecipesService, RecipeRepository],
 })
